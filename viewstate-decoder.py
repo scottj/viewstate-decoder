adcde988013b1d88c2bec59190e1e904c4f7e8e4
Scott Johnson <scott.johnson@gmail.com>
2021-02-23 20:29:56 -0600

    file handling, black formatting, test data

--- viewstate-decoder.py
+++ viewstate-decoder.py
@@ -2,42 +2,57 @@
 
 from viewstate import ViewState
 import argparse
+import sys
 import urllib.parse
 
-#Handling of command-line arguments
+# Handling of command-line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('-vs', dest='encoded_vs', help='Input Base64 ViewState')
-parser.add_argument('-f', dest='vs_filename', help='Input File with Base64 ViewState')
-parser.add_argument('-o', dest='output_filename', default='output_viewstate.txt', help='Name of the Output File with the Resulting ViewState')
+parser.add_argument("-vs", dest="encoded_vs", help="Base64 ViewState")
+parser.add_argument(
+    "-f",
+    dest="infile",
+    type=argparse.FileType("r"),
+    help="Input File with Base64 ViewState",
+)
+parser.add_argument(
+    "-o",
+    dest="outfile",
+    type=argparse.FileType("w"),
+    default=sys.stdout,
+    help="Name of the Output File with the Resulting ViewState",
+)
 args = parser.parse_args()
 
-encoded_vs = ''
+encoded_vs = ""
 
-if(not args.encoded_vs and not args.vs_filename):
-    print('ERROR: Input needed - Usage: viewstate-decoder.py [-h] [-vs ENCODED_VS] [-f VS_FILENAME] [-o OUTPUT_FILENAME]')
+if not args.encoded_vs and not args.infile:
+    print(
+        "Usage: viewstate-decoder.py [-h] [-vs ENCODED_VS] [-f VS_FILENAME] [-o OUTPUT_FILENAME]"
+    )
 else:
     # Check if the input is given in the command line
-    if (args.encoded_vs):
+    if args.encoded_vs:
         encoded_vs = args.encoded_vs
-    else: # If not, we have the input inside a file with name: args.vs_filename
-        input_file = open(args.vs_filename, 'r')
-        encoded_vs = input_file.read()
-        input_file.close()
+    else:  # If not, we have the input inside a file with name: args.infile
+        encoded_vs = args.infile.read()
+        args.infile.close()
 
         # Convert the URL encoded input into Base64 only (URL decode)
         encoded_vs = urllib.parse.unquote(encoded_vs)
 
-    #Create ViewState object
+    # Create ViewState object
     vs = ViewState(encoded_vs)
     decoded_vs = vs.decode()
     hmac = vs.mac
     sign = vs.signature
 
-    #Write to output file
-    output_file = open(args.output_filename, 'w')
-    output_file.write('Decoded Viewstate: ' + str(decoded_vs)) # We must set it as 'string' to write it because it's a tuple, not a string
-    output_file.write('\n ViewState HMAC Signature Type: ' + hmac)
-    output_file.write('\n ViewState HMAC Signature: ' + str(sign))
-    output_file.close()
-
-    print('Output file saved to: '+ args.output_filename)
+    # Write to output file
+    args.outfile.write(
+        "Decoded Viewstate: {}\n".format(str(decoded_vs))
+    )  # We must set it as 'string' to write it because it's a tuple, not a string
+    args.outfile.write("ViewState HMAC Signature Type: {}\n".format(hmac))
+    if hasattr(sign, "hex"):
+        args.outfile.write("ViewState HMAC Signature: {}\n".format(sign.hex()))
+    if args.outfile.name != "<stdout>":
+        print("Output file saved to: {}".format(args.outfile.name))
+    args.outfile.close()
